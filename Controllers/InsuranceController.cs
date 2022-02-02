952f12289bf8720bf74ecb355fa642e6607b4235
KacperWilk <kacper.wilk@microsoft.wsei.edu.pl>
2022-02-02 11:44:48 +0100

    Add project files.

---
+++ Controllers/InsuranceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkladkaUbezpieczenie.Models;
+
+namespace SkladkaUbezpieczenie.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        private static IList<Insurance> insurances = new List<Insurance>()
+        {
+            new Insurance()
+                {Id = 1,Email = "kacper.wilk@wsei.pl", BirthDate = new DateTime(2001, 12, 13), Price = 1000, YearlyLength = 3}
+        };
+        // GET: Insurance
+        public ActionResult Index()
+        {
+            return View(insurances);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Insurance());
+        }
+        [HttpPost]
+        public ActionResult Create(Insurance insurance)
+        {
+            insurance.Id = insurances.Count + 1;
+            insurances.Add(insurance);
+            return RedirectToAction("Details", new { id = insurance.Id });
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(insurances.FirstOrDefault(x => x.Id == id));
+        }
+
+    }
+}
---
+++ Models/Insurance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Routing.Constraints;
+
+namespace SkladkaUbezpieczenie.Models
+{
+    public class Insurance
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        [Display(Name = "Insurance Sum")]
+        public double Price { get; set; }
+        [Required]
+        [Display(Name = "Yearly Length")]
+        public double YearlyLength { get; set; }
+    }
+}
